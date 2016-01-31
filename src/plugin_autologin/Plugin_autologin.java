a7060ac035d684f5c5885e9141c30d914f3e052b
crashdemons <crashenator+gh@gmail.com>
2016-01-30 19:15:21 -0600

    initial project

---
+++ src/plugin_autologin/Plugin_autologin.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugin_autologin;
+import irtrusbot.*;
+/**
+ *
+ * @author crash
+ */
+public class Plugin_autologin extends IrcPlugin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
